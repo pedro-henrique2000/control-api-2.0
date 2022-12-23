43a98695970c22b6ee6087d75fd1e3d7f7e3174e
phb2000 <p.becker2000@hotmail.com>
2022-12-23 13:31:26 -0300

    initial commit

---
+++ src/main/java/com/project/controlfood/ControlFoodApplication.java
@@ -0,0 +1,13 @@
+package com.project.controlfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/controlfood/ControlFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.controlfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
